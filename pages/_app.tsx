f6e3b650d3a9c68675295873bcf75396eb18ac7f
Felipe <felipefloresvendas@gmail.com>
2022-06-04 23:07:46 -0300

    faet: update app.tsx add Layout component to render any component into

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { HealtBar } from "../components/HealtBar";
+import { Layout } from "../components/Layout";
+import { colors } from "./style";
+
+export const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+        <HealtBar />
+      </Layout>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
